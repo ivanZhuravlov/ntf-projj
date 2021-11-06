643a1ff1b80708a8f151e2322d2d70ee8d9e83d3
CamTosh <tochecamille@gmail.com>
2021-11-06 16:01:38 +0100

    add service page

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
+import Service from '../views/Service.vue'
 import Art from '../views/Art.vue'
 
 Vue.use(VueRouter)
@@ -17,6 +18,11 @@ const routes = [
     path: '/contact',
     name: 'Contact',
     component: Contact
+  },
+  {
+    path: '/service',
+    name: 'Service',
+    component: Service
   },
   {
     path: '/art/:id',
