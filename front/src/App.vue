b2affa46b114ff50661886cacca8b451fe973d7a
CamTosh <tochecamille@gmail.com>
2021-11-07 15:10:17 +0100

    fix tmp menu

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,8 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/contact">Contact</router-link>
-      <router-link to="/art/123">art</router-link>
+      <router-link to="/contact">Contact</router-link> | 
+      <router-link to="/art/123">Art example</router-link> |
+      <router-link to="/service">Service</router-link>
     </div>
     <router-view/>
   </div>
