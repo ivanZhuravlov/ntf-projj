45ed53143b634e903968e327c22945b4d46582b0
CamTosh <tochecamille@gmail.com>
2021-11-09 19:40:06 +0100

    add login & register

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/contact">Contact</router-link> | 
       <router-link to="/art/123">Art example</router-link> |
-      <router-link to="/service">Service</router-link>
+      <router-link to="/service">Service</router-link> |
+      <router-link to="/register">Register</router-link> |
+      <router-link to="/login">Login</router-link> 
     </div>
     <router-view/>
   </div>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import Home from '../views/Home.vue'
-import Contact from '../views/Contact.vue'
-import Service from '../views/Service.vue'
-import Art from '../views/Art.vue'
+import Home from '../views/Home.vue';
+import Contact from '../views/Contact.vue';
+import Service from '../views/Service.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Art from '../views/Art.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -20,6 +22,16 @@ const routes = [
     component: Contact
   },
   {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
     path: '/service',
     name: 'Service',
     component: Service
@@ -29,12 +41,12 @@ const routes = [
     name: 'Art',
     component: Art
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
