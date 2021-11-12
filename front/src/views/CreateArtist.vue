697d5cbc1b522cb32c0593cff65b01f8795a6bdd
Anniina <36889751+bitnose@users.noreply.github.com>
2021-11-12 22:02:38 +0200

    update CreateArtist
    Co-authored-by: Toche Camille <tochecamille@gmail.com>

--- front/src/views/CreateArtist.vue
+++ front/src/views/CreateArtist.vue
@@ -127,7 +127,7 @@ import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 
 export default {
-  name: 'Service',
+  name: 'CreateArtist',
   components: {
     Header,
     Footer,
