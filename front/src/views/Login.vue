0fe6e21fce8637b564b849342458131ac40c9d7f
CamTosh <tochecamille@gmail.com>
2021-11-14 20:25:00 +0100

    Register call API

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -3,6 +3,10 @@
     <Header />
     <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
       <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8">Sign in</h2>
+      
+      <div v-if='error' class="max-w-lg mx-auto bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-4" role="alert">
+        <span class="block sm:inline">{{ error }}</span>
+      </div>
 
       <div class="max-w-lg border rounded-lg mx-auto">
         <div class="flex flex-col gap-4 p-4 md:p-8">
@@ -13,7 +17,7 @@
 
           <div>
             <label for="password" class="inline-block text-gray-800 text-sm sm:text-base mb-2">Password</label>
-            <input v-model='password' name="password" class="w-full bg-gray-50 text-gray-800 border focus:ring-2 ring-blue-300 rounded outline-none transition duration-100 px-3 py-2" />
+            <input v-model='password' type='password' name="password" class="w-full bg-gray-50 text-gray-800 border focus:ring-2 ring-blue-300 rounded outline-none transition duration-100 px-3 py-2" />
           </div>
 
           <button @click='login()' class="block bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring focus:ring-2 ring-gray-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">
