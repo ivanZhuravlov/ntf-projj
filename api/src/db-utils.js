b9f5cd779a29c393d4466504c8ccb5817af3bf35
CamTosh <tochecamille@gmail.com>
2021-11-16 19:17:08 +0100

    fix active subscription

--- api/src/db-utils.js
+++ api/src/db-utils.js
@@ -2,12 +2,12 @@ const sql = require("./pg");
 
 async function findActiveSubscriptions(userId) {
     return sql`
-        select count(certificates.id), subscription_id, subscriptions.data->'product'->>'coaCount' as coaCount
-        from  certificates
-            inner join subscriptions on subscriptions.id = subscription_id 
-        where certificates.user_id = ${userId}
+        select count(certificates.id), subscriptions.id, subscriptions.data->'product'->>'coaCount' as coaCount
+        from subscriptions 
+            left join certificates on subscription_id = subscriptions.id
+        where subscriptions.user_id = ${userId}
             and subscriptions.terminated_at is null
-        group by subscription_id, coaCount;
+        group by subscriptions.id, coaCount;
         `;
 }
 
