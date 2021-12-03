7c23e192309774899694abfd2e66c7ef8ddb9fe7
AleksJenko <91942753+AleksJenko@users.noreply.github.com>
2021-12-03 19:18:53 +0100

    Update About.vue

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -7,12 +7,30 @@
       <div class="space-y-4">
         <h2 class="font-semibold capitalize text-xl place-items-center">About us</h2>
         <p class="text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          AFT in nutshell
+
+          Art Fruit Token is a Certificat of Authenticity, a unique digital objet Linked
+          to a physical Art piece. Whereas NFT (non fongible token) is a digital certificate
+          dedicated to digital Artpiece, its expansion generating a huge gap with the 
+          traditional world of Art. Jenko plateform decided to use the same technology to
+          allow Artist to be part of it without changing their Artistic practices.
+          
+          Art Fruit token aims to expand the Artist signature with a unique digital signature
+          of the Artpiece through an unique Digital Certificat of Authenticity that includes 
+          a dedicated contract able to reinforce the Intellectual property and New attributes 
+          as described below. Indeed, by allowing the transfer of this unique Digital Certificat
+          of Authenticity at the same time of the Artpiece, Art Fruit Token keeps the Artpiece
+          traceability and oblige the parties to respect the Artist wishes specified in the Contract
+          in particular the grant of Royalties on transaction, stating from the transaction between
+          the first and the second Artpiece Collector, and so on, without any limitations of 
+          Transaction nor period of time. Moreover, the control of Royalties would remain anytime 
+          possible by the Artist and transferable to having rights persons or foundation.
+
+          Furthermore, considering that a majority of Art piece are kept by the first buyer, 
+          Art Fruit Token is a powerfull tool to engage the Galleries, Agents, Collectors to resell
+          the Art piece with a Royalties incentive defined by the Artist. Through the platform the
+          Collector would be able to inform with discretion the availability of resell the Artpiece
+          and be connected to a physical or virtual Gallery or Auction platform registered in Jenko.
         </p>
 
         <h2 class="font-semibold capitalize text-xl place-items-center">Concept</h2>
