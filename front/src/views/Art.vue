40cb8b555da95b5e1e1c613e85272307778d3d3c
Anniina <anniina.korkiakangas@icloud.com>
2021-12-11 01:23:45 +0100

    Add royalties info on coa

--- front/src/views/Art.vue
+++ front/src/views/Art.vue
@@ -1,48 +1,73 @@
 <template>
   <div class="art">
     <Header />
-    <div class="container max-w-7xl sm:px-4 xs:px-2 mx-auto grid xs:grid-cols-1 sm:grid-cols-1 grid-cols-2 pt-6 gap-8">
-      <img class="max-w-screen-lg h-auto" :src="`https://ipfs.io/ipfs/${token.token_uri}`">
+    <div class=" bg-gray-50 mt-10 m-3 px-6 pb-5 rounded filter shadow-md container max-w-7xl sm:px-4 xs:px-3 mx-auto grid xs:grid-cols-1 sm:grid-cols-1 grid-cols-2 pt-6 gap-8 xs:gap-7 xs:justify-items-center">
+      <img class="max-w-screen-lg h-auto" :src="`${token.token_uri}`">
  
       <div>
-        <h1 class="text-gray-800 text-3xl font-bold capitalize leading-relaxed">{{ token.data.title }}</h1>
-        <h2 class="text-gray-500 capitalize">{{ token.artist.lastName }} {{ token.artist.firstName }}</h2>
-        <p class="text-gray-700">
+        <h1 class="text-gray-800 text-3xl font-bold capitalize leading-relaxed xs:text-center">{{ token.data.title }}</h1>
+        <h2 class="text-gray-700 capitalize xs:text-center font-semibold">{{ token.artist.lasName }} {{ token.artist.firstName }}</h2>
+        <p class="text-gray-700 xs:text-center">
           {{ token.data.description }}
         </p>
 
-        <div class="grid grid-cols-2 mt-12">
-          <div class="space-y-2">
-            <div v-if='token.data.artPieceId' class="uppercase">Art piece: {{ token.data.artPieceId }}</div>
-            <div v-if='token.data.createdAt' class="uppercase">Created At: {{ new Date(token.data.createdAt * 1000).toISOString().split('T')[0] }}</div>
-            <div v-if='token.data.size' class="uppercase">Size: {{ token.data.size }}</div>
-            <div v-if='token.data.material' class="uppercase">Material: {{ token.data.material }}</div>
-            <div v-if='token.data.technical' class="uppercase">Technical: {{ token.data.technical}}</div>
-            <div v-if='token.data.movment' class="uppercase">Movment: {{ token.data.movment}}</div>
+        <div class="grid grid-cols-2 mt-12 bg-white rounded-md xs:space-x-2 divide-x filter drop-shadow px-3">
+          <div class="space-y-4 space-y-2 px-2 my-3">
+            <div v-if='token.data.artPieceId' class="uppercase tracking-widest text-sm xs:text-xs">Art piece: {{ token.data.artPieceId }}</div>
+            <div v-if='token.data.createdAt' class="uppercase tracking-widest text-sm xs:text-xs">Created At: {{ new Date(token.data.createdAt * 1000).toISOString().split('T')[0] }}</div>
+            <div v-if='token.data.size' class="uppercase tracking-widest text-sm xs:text-xs">Size: {{ token.data.size }}</div>
+            <div v-if='token.data.material' class="uppercase tracking-widest text-sm xs:text-xs">Material: {{ token.data.material }}</div>
+            <div v-if='token.data.technical' class="uppercase tracking-widest text-sm xs:text-xs">Technical: {{ token.data.technical}}</div>
+            <div v-if='token.data.movment' class="uppercase tracking-widest text-sm xs:text-xs">Movment: {{ token.data.movment}}</div>
+            <div v-if='token.data.movment' class="uppercase tracking-widest text-sm xs:text-xs">Movment: {{ token.data.movment}}</div>
           </div>
-          <div class="space-y-4">
-            <div class="text-gray-500 uppercase">COA Transaction</div>
-            <a :href="'https://etherscan.io/tx/' + token.data.transaction" target="_blank" class="underline hover:text-blue-700">{{ clean(token.data.transaction) }}</a>
-            <div class="text-gray-500 uppercase">Owner Address</div>
-            <a :href="'https://etherscan.io/address/' + token.data.artistAddress" target="_blank" class="underline hover:text-blue-700">{{ clean(token.data.artistAddress) }}</a>
+          <div class="space-y-4 space-y-2 pl-5 my-3">
+            <div class="uppercase tracking-widest text-sm xs:text-xs">COA Transaction</div>
+            <a :href="'https://etherscan.io/tx/' + token.data.transaction" target="_blank" class="text-xs underline font-mono hover:text-blue-300">{{ clean(token.data.transaction) }}</a>
+            <div class=" uppercase tracking-widest text-sm xs:text-xs">Owner Address</div>
+            <a :href="'https://etherscan.io/address/' + token.data.artistAddress" target="_blank" class="text-xs font-mono underline hover:text-blue-300">{{ clean(token.data.artistAddress) }}</a>
           </div>
         </div>
       </div>
       
-      <div>
-        <h3 class="text-gray-800 text-2xl lg:text-3xl font-bold mb-4 md:mb-6">Research</h3>
-        <p class="text-gray-700 md:text-lg mx-auto">
+      <div class="xs:border-r-0 border-r-2 xs:text-center mt-10">
+        <h3 class="text-gray-800 text-2xl lg:text-2xl font-bold mb-4 md:mb-6 ">Certificate of Authenticity</h3>
+        <p class="text-gray-700 md:text-md text-sm mr-20 xs:mx-2">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
 
-      <div class="text-right">
-        <button class="mx-2 my-2 bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 px-6 py-2 text-xs focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600">
-          Download certificate
-        </button>
-      </div>
-    </div>
+      <div class="xs:bg-white filter drop-shadow rounded-md w-full py-3 px-6 xs:pb-6">
+        <h4 class="text-md font-semibold text-gray-700 ">Royalties Applied</h4>
+        <div class=" mt-5 flex flex-cols space-x-10">
+          <div v-if='token.royalties'>
+            <div v-if='token.royalties.roylatiesArtist' class="uppercase tracking-widest text-xs  ">Artist: {{ token.royalties.roylatiesArtist }} %</div>
+            <div v-if='token.royalties.roylatiesGallery' class="uppercase tracking-widest text-xs ">Gallery: {{ token.royalties.roylatiesGallery }}%</div>
+            <div v-if='token.royalties.roylatiesCollector0' class="uppercase tracking-widest text-xs">First Collector: {{ token.royalties.roylatiesCollector0 }} %</div> 
+            <div v-if='token.royalties.roylatiesCollector1' class=" uppercase tracking-widest text-xs">Second Collector: {{ token.royalties.roylatiesCollector1 }} %</div>
+            <div v-if='token.royalties.roylatiesCollector2' class="uppercase tracking-widest text-xs">Third Collector: {{ token.royalties.roylatiesCollector2 }} %</div>
+            <div v-if='token.royalties.roylatiesCollectorX' class="uppercase tracking-widest text-xs">X Collector: {{ token.royalties.roylatiesCollectorX }} %</div>
+            <div class="uppercase tracking-widest text-xs">Jenko: 3%</div>
 
+          </div>
+          <div v-else-if='$store.getters.isArtist' class="h-full">
+            <p class="text-sm text-gray-500 pb-3 mb-6">Jenko: 3%</p> 
+            <router-link :to='`/royalties/${token.id}`' class="mt-6 my-10 bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 px-6 py-2 text-xs focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600">
+            Add Royalties
+            </router-link>
+          </div>
+          <div v-else>
+            <p class="text-sm text-gray-500 pb-3 mb-6">Jenko: 3%</p>
+          </div>
+        </div>           
+      </div>
+        <div class="text-right col-span-full justify-center flex pt-10">
+          <button class="mx-2 my-2 mr-10  bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 px-6 py-2 text-xs focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600 xs:mx-2">
+           Download certificate
+          </button>
+        </div>
+    </div>
+    
     <Footer />
   </div>
 </template>
@@ -60,7 +85,7 @@ export default {
   },
   data() {
     return {
-      token: {}
+      token: { }  
     }
   },
   async mounted() {
@@ -78,7 +103,7 @@ export default {
     },
     clean(longText) {
       return longText.slice(0, 8) + ' ... ' + longText.slice(-8);
-    }
+    },
   }
 }
 </script>
