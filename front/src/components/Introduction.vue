14268aedbe7a3dcda27b7afcbf225773a97b5691
Anniina <anniina.korkiakangas@icloud.com>
2022-04-23 12:10:52 +0200

    update landing

--- front/src/components/Introduction.vue
+++ front/src/components/Introduction.vue
@@ -1,37 +1,48 @@
 <template>
- <div class="flex flex-col items-center lg:flex-row gap-6 mt-36 space-x-4">
-      <div class="px-10 mb-3">
-        <div class="flex place-content-center pb-3">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-          </svg>
+  <div class="px-10 py-10">
+    <div class="border-t border-b border-darkerGray py-10 lg:mx-16">
+      <div class="flex-row lg:flex items-top gap-16 mb-10">
+        <!-- Card 1 - start -->
+        <div class="pb-10">
+          <div class="flex place-content-center pb-4 gap-2.5">
+            <img class="h-auto w-10" src="../assets/icons/protect.svg">
+            <h1 class="font-light text-2xl text-center text-fontGray">PROTECT YOUR ART </h1>
+          </div>
+          <p class="font-normal text-justify tracking-normal leading-6 text-textGray max-w-prose">Secure your Artwork
+            with an unique AFT Certificate of Authenticity recorded in Blockchain. Ensure your Collectors it’s from you.
+            Authorize the Gallery to create certificates on your behalf.</p>
         </div>
-        <h1 class="font-bold mono text-center font-mono uppercase font-sm">Register</h1>
-        <p class="font-xs font-thin text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua.</p>
+        <!-- Card 1 - end -->
+        <!-- Card 2 - start -->
+        <div class="pb-10">
+          <div class="flex place-content-center pb-4 gap-2.5">
+            <img class="h-auto w-10" src="../assets/icons/transfer.svg">
+            <h1 class="font-light text-2xl text-center text-fontGray">TRANSFER & REWARD</h1>
+          </div>
+          <p class="font-normal text-justify tracking-normal leading-6 text-textGray max-w-prose">Transfer easily the
+            Certificate of Authenticity to Gallery and Collector & keep a complete traceability of your Artwork. Reward
+            value makers (such as the Gallery and the Collector) with Royalties. discover our AFT Agreement</p>
+        </div>
+        <!-- Card 2 - end -->
+        <!-- Card 3 - start -->
+        <div class="pb-10">
+          <div class="flex place-content-center pb-4 gap-2.5">
+            <img class="h-auto w-10" src="../assets/icons/manage.svg">
+            <h1 class="font-light text-2xl text-center text-fontGray">MANAGE YOUR ASSETS</h1>
+          </div>
+          <p class="font-normal text-justify tracking-normal leading-6 text-textGray text-base max-w-prose">Get the full
+            picture of your assets in the Dashboard, manage easily the transfer of AFT Certificate of Authenticity and
+            Fruit your Artwork with our Royalties System provided.</p>
+        </div>
+        <!-- Card 3 - end -->
       </div>
-
-      <div class="px-10 mb-3">
-        <div class="flex place-content-center pb-3">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-          </svg>
-        </div>
-        <h1 class="font-bold uppercase font-mono text-center font-sm">Verify your art</h1>
-        <p class="font-xs font-thin text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
-
-      <div class="px-10 mt-0">
-        <div class="flex place-content-center pb-3">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
-          </svg>
-        </div>
-        <h1 class="font-bold uppercase font-mono text-center font-sm">Collect royalties</h1>
-        <p class="font-xs font-thin text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua.</p>
+      <div class="flex justify-center">
+        <button type="submit"
+          class="inline-block px-6 py-4 bg-orange text-white font-medium text-xs leading-tight uppercase rounded-md shadow-md hover:bg-gray-600 hover:shadow-lg transition duration-200 ease-in-out">Get
+          started</button>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
