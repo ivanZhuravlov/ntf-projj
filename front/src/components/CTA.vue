5bba739f2242b39cbbda2bff5cd752cf2d3ddeff
Anniina <anniina.korkiakangas@icloud.com>
2022-04-23 12:06:46 +0200

    add icons

---
+++ front/src/components/CTA.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="px-10 py-14 flex-row w-screen items-top justify-evenly lg:flex">
+    <!-- AFT certificate - start -->
+    <div class="h-max-600 pb-14 lg:w-max-600 ">
+      <img src="../assets/images/AFT_home.png" alt="AFT_home">
+    </div>
+    <!-- AFT certificate - end -->
+
+    <!-- Hero - start -->
+    <div class="max-w-2xl space-y-2 lg:space-y-6">
+      <div class="text-center space-y-1 tracking-wide pb-10 lg:space-y-4">
+        <p class="uppercase text-2xl font-extralight lg:text-5xl text-mediumGray"> Our <span
+            class="text-orange font-medium"> AFT </span> Certificate </p>
+        <p class="text-orange text-xl font-medium lg:text-4xl">dedicated to physical Artwork</p>
+      </div>
+      <p class="text-mediumGray text-base  font-thin h-36 text-justify tracking-wide lg:leading-9 lg:text-2xl">
+        This <span class="text-gray-600 font-normal">Certificate of Authenticity</span> includes a powerfull combination
+        of <span class="text-gray-600 font-normal uppercase"> NFT & Royalties </span> while improving your intellectual
+        Property Rights<br />
+      </p>
+      <p class="text-mediumGray text-base font-thin h-36 texts-justify tracking-wide lg:text-2xl lg:leading-9">
+        Build the Trust around your Artwork with a unique and irrefutable proof to secure your creation and sustain the
+        traceability of your Art forever.
+      </p>
+      
+      <!-- Button - start -->
+      <div class="flex justify-center">
+        <button type="submit"
+          class="px-6 py-4 bg-gray-700 text-white font-medium text-xs uppercase rounded-md shadow-md hover:bg-gray-600 hover:shadow-lg transition duration-200 ease-in-out">Learn
+          more</button>
+      </div>
+    </div>
+     <!-- Button - end -->
+    <!-- Hero - end -->
+  </div>
+</template>
+<script>
+  export default {
+    name: 'AFTPreview',
+  };
+</script>
