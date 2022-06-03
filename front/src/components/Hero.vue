32d0b95e6263510a311e4e71294c4bae1f5d8682
CamTosh <tochecamille@gmail.com>
2022-06-03 19:31:25 +0200

    Fix hero

--- front/src/components/Hero.vue
+++ front/src/components/Hero.vue
@@ -1,32 +1,26 @@
 <template>
-  <section class="h-400 sm:h-600 md:h-screen lg:h-screen bg-local bg-cover bg-hero w-full">
-    <div class="px-4 pt-10 lg:pt-0">
-      <div class="flex-row justify-evenly items-center sm:px-10 md:justify-around lg:h-screen lg:flex">
-        <div>
-          <!-- Text - start -->
-          <h1 class="text-orange uppercase tracking-wide font-bold text-2xl lg:text-6xl lg:font-semibold">Art Fruit
-            Token</h1>
-          <p class="text-mediumGray uppercase tracking-wide mt-2 font-bold text-xl lg:text-5xl lg:font-semibold">
-            Certificate of Authenticity</p>
-          <p class="text-mediumGray font-medium text-sm my-3 lg:my-10 lg:text-2xl">Fruit your physical Artworks with
-            value makers</p>
-          <!-- Text - end -->
-          <!-- Button - start -->
-          <div class="flex justify-center pt-28 lg:justify-start">
-            <button type="submit"
-              class="px-6 py-4 bg-orange text-white font-medium text-xs leading-tight uppercase rounded-md shadow-md hover:bg-gray-600 hover:shadow-lg transition duration-200 ease-in-out">Get
-              started</button>
-          </div>
-          <!-- Button - end -->
-        </div>
-        <div class="-mt-28 lg:mt-40">
-          <h1
-            class="uppercase text-white text-right font-bold text-xl md:place-self-start lg:font-extralight lg:text-5xl lg:place-self-auto ">
-            NFTs unlike anothers </h1>
-        </div>
+  <div class="h-400 sm:h-600 md:h-screen lg:h-screen bg-local bg-cover bg-hero w-full flex justify-center items-center">
+    <div class="w-4/5 max-w-9xl">
+      <h1 class="text-orange uppercase tracking-wide text-2xl lg:text-6xl">Art Fruit
+        Token
+      </h1>
+      <p class="text-mediumGray uppercase tracking-wide mt-2 text-xl lg:text-5xl">
+        Certificate of Authenticity
+      </p>
+      <p class="text-mediumGray text-sm my-3 lg:my-10 lg:text-2xl">Fruit your physical Artworks with
+        value makers
+      </p>
+      <div class="w-full flex justify-between">
+        <button type="submit" class="px-6 py-4 bg-orange text-white text-xs leading-tight uppercase rounded-md shadow-md hover:bg-gray-600 hover:shadow-lg transition duration-200 ease-in-out">
+          Get started
+        </button>
+        <h1 class="uppercase text-white text-right text-xl md:place-self-start lg:font-extralight lg:text-5xl lg:place-self-auto">
+          NFTs unlike anothers
+        </h1>
       </div>
     </div>
-  </section>
+  </div>
+  
 </template>
 
 <script>
