8bfe98bc261be5a5df3875d1c48e8848dff2c0d1
CamTosh <tochecamille@gmail.com>
2022-06-25 18:36:47 +0200

    update menu

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -86,25 +86,30 @@
     <!--  Dashboard menu - end -->
 
     <!--  Dropdown menu - start -->
-    <div v-if="showMenu" class="absolute mt-72 right-5 bg-white text-base list-none text-center rounded border border-gray-100 filter drop-shadow-2xl space-y-2">
+    <div v-if="showMenu" class="absolute mt-64 right-5 bg-white text-base list-none text-center rounded border border-gray-100 filter drop-shadow-2xl space-y-2">
       <router-link v-if='!isConnected' to="/login" class="link pt-4">
-        Login
+        <svg class="w-4 h-4 rounded-full border border-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+        <div class="ml-2">Get Started</div>
       </router-link>
-      <router-link v-if='!isConnected' to="/register" class="link">
-        Register
+
+      <router-link to="/dashboard" class="link">
+        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="22" y1="12" x2="18" y2="12"></line><line x1="6" y1="12" x2="2" y2="12"></line><line x1="12" y1="6" x2="12" y2="2"></line><line x1="12" y1="22" x2="12" y2="18"></line></svg>
+        <div class="ml-2">Dashboard</div>
       </router-link>
-      <router-link v-else to="/profile" class="link">
-        Profile
+      
+      <router-link to="/profile" class="link">
+        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
+        <div class="ml-2">Settings</div>
       </router-link>
-      <router-link to="/contact" class="link">
-        Contact
-      </router-link>
+      
       <button v-if='isConnected' @click="logout()" to="/logout" class="link">
-        Logout
+        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+        <div class="ml-2">Logout</div>
       </button>
       <hr class="">
       <router-link to="/about" class="link">
-        About
+        <img class="w-4 h-4" src="../assets/icons/logo.svg" alt="">
+        <div class="ml-2">About</div>
       </router-link>
     </div>
     <!--  Dropdown menu - end -->
@@ -113,7 +118,7 @@
 
 <style>
   .link {
-    @apply text-sm py-2 px-10 font-semibold block w-full whitespace-nowrap bg-transparent text-gray-800 hover:bg-gray-100 hover:bg-opacity-75;
+    @apply text-sm py-2 pl-4 pr-8 font-semibold w-full whitespace-nowrap bg-transparent text-gray-800 hover:bg-gray-100 hover:bg-opacity-75 flex items-center;
   }
 </style>
 
