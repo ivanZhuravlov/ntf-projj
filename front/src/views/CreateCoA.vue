c6eb72157242a793b776f34737ed8b1803d69f5e
CamTosh <tochecamille@gmail.com>
2022-06-26 19:27:49 +0200

    tile

--- front/src/views/CreateCoA.vue
+++ front/src/views/CreateCoA.vue
@@ -21,7 +21,7 @@
 
         <div>
           <div>
-            <label class="inline-block text-sm sm:text-base mt-6 mb-1">Name of the Artpiece *</label>
+            <label class="inline-block text-sm sm:text-base mt-6 mb-1">Title *</label>
             <input required v-model="title" class="w-full bg-gray-50 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
           </div>
 
